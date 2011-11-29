ee3f27e3609c9e7a0a5dfaf35159f3346f339ed8
NathanGloyn <nathans.dropbox@gmail.com>
2011-11-29 22:38:13 +0000

    Altered solution removing the NDepend details
    Altered the main data access interface renaming parameter storedProcedure to commandText
    Added NUnit via NuGet

--- ADO.Net.DataAccessLayer/Interfaces/IDataAccess.cs
+++ ADO.Net.DataAccessLayer/Interfaces/IDataAccess.cs
@@ -21,37 +21,37 @@ namespace DataAccessLayer.Interfaces
         /// <summary>
         /// Executes a command and returns a DataTable
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">Optional DbParameter collection to use in executing</param>
         /// <returns>DataTable populated with data from executing stored procedure</returns>
-        DataSet ExecuteDataSet(string storedProcedureName, params DbParameter[] parameters);
+        DataSet ExecuteDataSet(string commandText, params DbParameter[] parameters);
 
 
         /// <summary>
         /// Executes a command and returns a DataTable
         /// </summary>
         /// <param name="cmd">Output parameter that holds reference to the command object just executed</param>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">SqlParameter collection to use in executing</param>
         /// <returns>DataTable populated with data from executing stored procedure</returns>
-        DataSet ExecuteDataSet(out DbCommand cmd, string storedProcedureName, params DbParameter[] parameters);
+        DataSet ExecuteDataSet(out DbCommand cmd, string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command and returns a DataTable
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">Optional DbParameter collection to use in executing</param>
         /// <returns>DataTable populated with data from executing stored procedure</returns>
-        DataTable ExecuteDataTable(string storedProcedureName, params DbParameter[] parameters);
+        DataTable ExecuteDataTable(string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command and returns a DataTable
         /// </summary>
         /// <param name="cmd">Output parameter that holds reference to the command object just executed</param>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">SqlParameter collection to use in executing</param>
         /// <returns>DataTable populated with data from executing stored procedure</returns>
-        DataTable ExecuteDataTable(out DbCommand cmd, string storedProcedureName, params DbParameter[] parameters);
+        DataTable ExecuteDataTable(out DbCommand cmd, string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command that does not return a query
@@ -64,53 +64,53 @@ namespace DataAccessLayer.Interfaces
         /// <summary>
         /// Executes a command that does not return a query
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">DbParameter collection to use in executing</param>
         /// <param name="cmd">Output parameter that will return the command object used in the execution of the procedure</param>
         /// <returns>Integer holding the number of rows effected by the stored procedure execution</returns>
-        int ExecuteNonQuery(out DbCommand cmd, string storedProcedureName, params DbParameter[] parameters);
+        int ExecuteNonQuery(out DbCommand cmd, string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command and returns a data reader
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">DbParameter collection to use in executing</param>
         /// <returns>SqlDataReader allowing access to results from command</returns>
-        DbDataReader ExecuteReader(string storedProcedureName, params DbParameter[] parameters);
+        DbDataReader ExecuteReader(string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command that returns a single value
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">DbParameter collection to use in executing</param>
         /// <returns>Object holding result of execution of database</returns>
-        object ExecuteScalar(string storedProcedureName, params DbParameter[] parameters);
+        object ExecuteScalar(string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command that returns a single value
         /// </summary>
         /// <param name="cmd">Output parameter that holds reference to the command object just executed</param>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">DbParameter collection to use in executing</param>
         /// <returns>Object holding result of execution of database</returns>
-        object ExecuteScalar(out DbCommand cmd, string storedProcedureName, params DbParameter[] parameters);
+        object ExecuteScalar(out DbCommand cmd, string commandText, params DbParameter[] parameters);
 
  
         /// <summary>
         /// Executes a command and returns an XML reader.
         /// </summary>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">Optional DbParameter collection to use in executing</param>
         /// <returns>An instance of XmlReader pointing to the stream of xml returned</returns>
-        XmlReader ExecuteXmlReader(string storedProcedureName, params DbParameter[] parameters);
+        XmlReader ExecuteXmlReader(string commandText, params DbParameter[] parameters);
 
         /// <summary>
         /// Executes a command and returns an XML reader.
         /// </summary>
         /// <param name="cmd">Output parameter that holds reference to the command object just executed</param>
-        /// <param name="storedProcedureName">command text to execute</param>
+        /// <param name="commandText">command text to execute</param>
         /// <param name="parameters">DbParameter collection to use in executing</param>
         /// <returns>An instance of XmlReader pointing to the stream of xml returned</returns>
-        XmlReader ExecuteXmlReader(out DbCommand cmd, string storedProcedureName, params DbParameter[] parameters);
+        XmlReader ExecuteXmlReader(out DbCommand cmd, string commandText, params DbParameter[] parameters);
     }
 }
